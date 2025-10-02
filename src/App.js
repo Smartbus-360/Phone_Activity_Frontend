331ae07686be5b3211ea46b4b51474e4cfd85b2e
Smartbus-360 <smartbus365@gmail.com>
2025-10-02 20:27:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
+// import React from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import AssignDrivers from "./pages/AssignDrivers";
+// import HomePage from "./pages/Homepage";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="Homepage /"
+//         <Route path="/" element={<Login />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//         <Route path="/assign-drivers" element={<AssignDrivers />} />
+
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import AssignDrivers from "./pages/AssignDrivers";
+import HomePage from "./pages/Homepage"; // ✅ make sure file name matches
+import CreateSchoolAdmin from "./pages/CreateSchoolAdmin";
+import CreateSchool from "./pages/CreateSchool";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* ✅ Homepage route */}
+        <Route path="/homepage" element={<HomePage />} />
+
+        {/* ✅ Login as root route */}
         <Route path="/" element={<Login />} />
+
+        {/* ✅ Other routes */}
         <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/assign-drivers" element={<AssignDrivers />} />
+        <Route path="/assign-drivers" element={<AssignDrivers />} />
+        <Route path="/create-school-admin" element={<CreateSchoolAdmin />} />
+        <Route path="/create-school" element={<CreateSchool />} />
 
       </Routes>
     </BrowserRouter>
