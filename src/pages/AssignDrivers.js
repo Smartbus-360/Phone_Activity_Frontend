00c2dd8f58268842a15511b559342e6c8117e830
Smartbus-360 <smartbus365@gmail.com>
2025-10-16 09:36:23 +0530

    Update AssignDrivers.js

--- src/pages/AssignDrivers.js
+++ src/pages/AssignDrivers.js
@@ -407,7 +407,7 @@ export default function AssignDrivers() {
 const [editModalOpen, setEditModalOpen] = useState(false);
 const [editingDriver, setEditingDriver] = useState(null);
   const [searchText, setSearchText] = useState("");
-  // const [form] = Form.useForm();
+  const [form] = Form.useForm();
   const [createForm] = Form.useForm();
 const [editForm] = Form.useForm();
 
